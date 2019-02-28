1c3007220d46ae2a324b8f62812bb508c0619fe8
KevinChen1994 <346521888@qq.com>
2019-02-28 11:16:47 +0800

    test

---
+++ test.py
@@ -0,0 +1,11 @@
+#-*- coding:utf-8 -*-
+# author:chenmeng
+# datetime:2019/2/21 11:02
+# software: PyCharm
+
+import random
+
+
+
+start_id = random.randint(0, 100 - 2 * 5 - 10)
+end_id = start_id + 100 * 5 + 100
